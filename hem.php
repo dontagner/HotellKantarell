cfd3747e0e86b64fd95e36a127ca57f3fdde1fc0
dontagner <eltontagner@gmail.com>
2025-03-24 11:46:08 +0100

    Framsida med toppbar start

---
+++ hem.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotell Kantarell</title>
+    <link rel="stylesheet" href="hem.css">
+</head>
+<body>
+
+<div class="topbar">
+    <div class="imgbox">
+        <img src="Hotell Kantarell.jpg" alt="Hotell Kantarell">
+    </div>
+    <div class="text">
+        <h1>Hotell Kantarell</h1>
+        <div class="navbar">
+        <a href="Hem.php">Hem</a>
+        <a href="Boka.php">Boka</a>
+        <a href="Om.php">Om</a>
+        <a href="Kontakt.php">Kontakt</a>
+    </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
