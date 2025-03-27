40c3f55403c13d5e7c344bc1ccc237bc4671f665
dontagner <eltontagner@gmail.com>
2025-03-27 09:39:25 +0100

    foenem

---
+++ kontakt.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakta oss</title>
+    <link rel="stylesheet" href="kontakt.css">
+</head>
+<body>
+<div class="topbar">
+    <div class="imgbox">
+        <img src="Hotell Kantarell.jpg" alt="Hotell Kantarell">
+    </div>
+    <div class="text">
+        <h1>Hotell Kantarell</h1>
+        <div class="navbar">
+        <a href="hem.html">Hem</a>
+        <a href="om.html">Om</a>
+        <a href="boka.php">Boka</a>
+    </div>
+</div>
+</div>
+</body>
+</html>
